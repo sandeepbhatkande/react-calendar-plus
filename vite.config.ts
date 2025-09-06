08dc12580047ef05ac7bd541b3bb4b85224c49b4
Sandeep Bhatkande <sandeepbhatkande72@gmail.com>
2025-09-06 16:17:26 +0530

    Fixing path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: './src/demo',
+  base: '/react-calendar-plus/',
   resolve: {
     alias: {
       '@': '/src',
